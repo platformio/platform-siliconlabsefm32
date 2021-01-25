55e02a97ee499aa192025b8afc8f513de659778e
valeros <valeros08@gmail.com>
2021-01-25 12:14:24 +0200

    Declare optional package versions

--- platform.py
+++ platform.py
@@ -41,7 +41,6 @@ class Siliconlabsefm32Platform(PlatformBase):
                 if p.startswith("framework-zephyr-") or p in (
                     "tool-cmake", "tool-dtc", "tool-ninja"):
                     self.packages[p]["optional"] = False
-            self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.80201.0"
             if "windows" not in get_systype():
                 self.packages["tool-gperf"]["optional"] = False
 
