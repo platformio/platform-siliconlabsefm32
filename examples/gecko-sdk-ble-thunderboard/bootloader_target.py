3e9dcdae56f5d4bca0138321ebe1d45abbcf9030
valeros <valeros08@gmail.com>
2021-12-22 14:10:52 +0200

    Update custom target in BLE example

--- examples/gecko-sdk-ble-thunderboard/bootloader_target.py
+++ examples/gecko-sdk-ble-thunderboard/bootloader_target.py
@@ -8,13 +8,13 @@ if "idedata" in COMMAND_LINE_TARGETS:
     env.Exit(0)
 
 
-BOOTLOADER_BINARY = "brd4184a_boot.bin"
-assert os.path.isfile(BOOTLOADER_BINARY), "Missing bootloader binary!"
+custom_bootloader_bin = env.GetProjectOption("custom_bootloader_bin", "")
+assert os.path.isfile(custom_bootloader_bin), "Missing bootloader binary!"
 
 if "program_bootloader" in COMMAND_LINE_TARGETS:
     env.Replace(
         UPLOADCMD='$UPLOADER $UPLOADERFLAGS -CommanderScript "${__jlink_cmd_script(__env__, "%s")}"'
-        % BOOTLOADER_BINARY
+        % custom_bootloader_bin
     )
 
     env.BoardConfig().update("upload.offset_address", "0x0")
@@ -23,7 +23,7 @@ env.AddCustomTarget(
     "program_bootloader",
     None,
     env.VerboseAction("$UPLOADCMD", "Programming bootloader..."),
-    title=None,
-    description=None,
+    title="Program bootloader",
+    description="A custom target for programming bootloader binary within a standalone project",
     always_build=True,
 )
