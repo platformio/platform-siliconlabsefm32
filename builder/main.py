f4bcc3b93566b3a13cf99c337d40fb2d671a9218
Ivan Kravets <me@ikravets.com>
2022-05-28 16:41:09 +0300

    Remove unnecessary files

--- builder/main.py
+++ builder/main.py
@@ -21,7 +21,6 @@ from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default,
                           DefaultEnvironment)
 
 env = DefaultEnvironment()
-env.SConscript("compat.py", exports="env")
 platform = env.PioPlatform()
 
 env.Replace(
